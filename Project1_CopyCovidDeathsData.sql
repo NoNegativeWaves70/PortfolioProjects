c9470315c5bcc0b6150db9072c9dc4bef6fb9603
Bruce Beck <NoNegativeWaves70@gmail.com>
2023-08-06 22:25:15 -0700

    Add files via upload
    Initial commit.

---
+++ Project1_CopyCovidDeathsData.sql
@@ -0,0 +1,4 @@
+COPY sqlproject.CovidDeaths
+FROM '/Users/brucebeck/Desktop/RDBMS_Work/ATA_PortfolioProject1/CovidDeaths.csv'
+DELIMITER ','
+CSV HEADER;
---
+++ Project1_CopyCovidVaccintionsData.sql
@@ -0,0 +1,4 @@
+COPY SqlProject.CovidVaccinations
+FROM '/Users/brucebeck/Desktop/RDBMS_Work/ATA_PortfolioProject1/CovidVaccinations.csv'
+DELIMITER ','
+CSV HEADER;
